05dcc2170bc8a124e6eb91c01ab46fdcacb909e4
nazzyadmin <santosmervin92@gmail.com>
2026-05-06 00:18:17 -0400

    Change loader URL in jbs.lua
    Updated the loader URL for the script.

--- jbs.lua
+++ jbs.lua
@@ -3,4 +3,4 @@
 getgenv().NoSilentAim = false -- To load without silent aim or not, (true/false)
 getgenv().VehicleFlingPower = 2710400 -- The power of both Vehicle Flings (changing this number after execution will require disabling and reenabling the toggle)
 getgenv().FlingPredictionAmount = 2 -- The amount of prediction auto flings (Fling Target/Nearest buttons) should use.
-loadstring(game:HttpGet("https://jewhack.org/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nazzyadmin/antl/refs/heads/main/antl.lua"))()
